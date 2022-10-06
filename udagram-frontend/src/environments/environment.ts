c28e0636c797ca301582eb993c274131f2fcda35
Dao Le Viet Hoang <daoleviethoang@gmail.com>
2022-10-06 14:50:19 +0700

    add kubernetes and config deploy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a855a53a5abcf472aa6f87057ddb3676-896113086.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
